d7e47b1f211bad7694c2f42ac3ec7a25a048287d
jasonwet <jason@colonialpress.net>
2020-11-02 22:28:19 -0500

    created basic command structure

---
+++ src/commands/information/role.js
@@ -0,0 +1,14 @@
+const Discord = module.require('discord.js');
+const toggles = require('../../savagex/toggles.json')
+const messages = require('../../savagex/messages.json')
+
+module.exports.run = (client, message, args) => {
+    if (toggles.information.role !== true) return message.channel.send(':x: Command Disabled')
+
+    message.channel.send(message.guild.name)
+}
+
+module.exports.config = {
+    command: 'role',
+    aliases: []
+}
