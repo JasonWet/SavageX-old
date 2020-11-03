a41d22d8fd2b7fc36fe5ed4b128995d2bc05c203
jasonwet <jason@colonialpress.net>
2020-11-02 22:31:36 -0500

    setup command structure for moderation commands

---
+++ src/commands/moderation/ban.js
@@ -0,0 +1,14 @@
+const Discord = module.require('discord.js');
+const toggles = require('../../savagex/toggles.json')
+const messages = require('../../savagex/messages.json')
+
+module.exports.run = (client, message, args) => {
+    if (toggles.moderation.ban !== true) return message.channel.send(':x: Command Disabled')
+
+    message.channel.send(message.guild.name)
+}
+
+module.exports.config = {
+    command: 'ban',
+    aliases: []
+}
