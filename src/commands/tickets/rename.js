81f5b8823dad052aa39f03e3304992eb0b92fac0
jasonwet <jason@colonialpress.net>
2020-11-04 14:23:05 -0500

    added toggle check

--- src/commands/tickets/rename.js
+++ src/commands/tickets/rename.js
@@ -2,6 +2,8 @@ const Discord = module.require('discord.js');
 const conf = require('../../conf.json')
 
 module.exports.run = (client, message, args) => {
+    if (conf.toggles.tickets.rename !== true) return message.channel.send(':x: Command Disabled');
+
     message.channel.send('RENAME')
 }
 
