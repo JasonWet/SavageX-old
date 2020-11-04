c01dd4e75fef45f36d81d5a7b41a99ce0daa7143
jasonwet <jason@colonialpress.net>
2020-11-04 14:19:02 -0500

    ticket remove command

--- src/commands/tickets/remove.js
+++ src/commands/tickets/remove.js
@@ -1,7 +1,23 @@
 const Discord = module.require('discord.js');
+const conf = require('../../conf.json')
 
 module.exports.run = (client, message, args) => {
-    message.channel.send('REMOVE')
+    if (conf.toggles.tickets.add !== true) return message.channel.send(':x: Command Disabled');
+
+    let id = args[0];
+
+    let mentioned = message.mentions.members.first() || message.guild.members.cache.get(id);
+    if (!mentioned) return message.channel.send(':x: You must specify a user')
+
+    if (message.channel.parent.id !== conf.categories.ticketCategory) return message.channel.send(':x: You can only use this command in a ticket channel')
+
+    message.channel.updateOverwrite(mentioned.id, {
+        "SEND_MESSAGES": false,
+        "VIEW_CHANNEL": false,
+        "READ_MESSAGE_HISTORY": false
+    }).then(() => {
+        message.channel.send(mentioned + 'has been removed from `' + message.channel.name + '` by ' + message.author)
+    })
 }
 
 module.exports.config = {
