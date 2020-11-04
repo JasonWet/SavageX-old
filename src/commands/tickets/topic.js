dce58442d9a3b074b09609a2fe0c9c39084d53c0
jasonwet <jason@colonialpress.net>
2020-11-04 14:27:43 -0500

    topic command created

--- src/commands/tickets/topic.js
+++ src/commands/tickets/topic.js
@@ -4,7 +4,15 @@ const conf = require('../../conf.json')
 module.exports.run = (client, message, args) => {
     if (conf.toggles.tickets.topic !== true) return message.channel.send(':x: Command Disabled');
 
-    message.channel.send('TOPIC')
+    if (message.channel.parent.id !== conf.categories.ticketCategory) return message.channel.send(':x: You can only use this command in a ticket channel')
+
+    if (!args) return message.channel.send(':x: You must provide a topic')
+
+    let topic = args.join(" ");
+
+    message.channel.setTopic(`${topic}`).then(() => {
+        message.channel.send(message.author + 'has set the topic to `' + topic + '`')
+    })
 }
 
 module.exports.config = {
