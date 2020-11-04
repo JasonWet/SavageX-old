07022cf47dcac1dfe0c3cf91566271096275707e
jasonwet <jason@colonialpress.net>
2020-11-04 14:18:23 -0500

    ticket add command

--- src/commands/tickets/add.js
+++ src/commands/tickets/add.js
@@ -1,7 +1,23 @@
 const Discord = module.require('discord.js');
+const conf = require('../../conf.json')
 
 module.exports.run = (client, message, args) => {
-    message.channel.send('ADD')
+    if (conf.toggles.tickets.add !== true) return message.channel.send(':x: Command Disabled');
+
+    let id = args[0];
+
+    let mentioned = message.mentions.members.first() || message.guild.members.cache.get(id);
+    if (!mentioned) return message.channel.send(':x: You must specify a user')
+
+    if (message.channel.parent.id !== conf.categories.ticketCategory) return message.channel.send(':x: You can only use this command in a ticket channel')
+
+    message.channel.updateOverwrite(mentioned.id, {
+        "SEND_MESSAGES": true,
+        "VIEW_CHANNEL": true,
+        "READ_MESSAGE_HISTORY": true
+    }).then(() => {
+        message.channel.send(mentioned + 'has been added to `' + message.channel.name + '` by ' + message.author)
+    })
 }
 
 module.exports.config = {
