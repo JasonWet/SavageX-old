6a295ce7fc12c1d0c5aea539006d8868731af203
jasonwet <jason@colonialpress.net>
2020-11-03 21:50:02 -0500

    updated console message

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -4,7 +4,7 @@ const conf = require('../conf.json');
 
 client.on(`guildMemberRemove`, async member => {
     let channel = member.guild.channels.cache.get(conf.channels.leaveLogsChannel);
-    if (!channel) return console.log(`${member.guild.name} - Leave Logs Channel not set...`);
+    if (!channel) return console.log(`${message.guild.name} - Leave Logs Channel not set...` && `${message.guild.name} - Join discord.gg/savagelabs for support`)
 
     const embed = new Discord.MessageEmbed()
         .setTitle(`Leave (${member.guild.memberCount})`)
